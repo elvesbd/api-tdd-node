b23e480dd59e354a00fe17a5b34f42dbe7c9bbba
elvesbd <elvesbd@yahoo.com>
2021-11-21 19:45:37 -0300

    chore: ensure loggin router call AuthUseCase with correct params

--- src/presentation/routers/login-routes.spec.js
+++ src/presentation/routers/login-routes.spec.js
@@ -2,12 +2,23 @@ const LoginRouter = require('./login-router')
 const MissingParamError = require('../helpers/missing-param-error')
 
 const makeSut = () => {
-  return new LoginRouter()
+  class AuthUseCaseMock {
+    auth (email, password) {
+      this.email = email
+      this.password = password
+    }
+  }
+  const authUseCaseMock = new AuthUseCaseMock()
+  const sut = new LoginRouter(authUseCaseMock)
+  return {
+    sut,
+    authUseCaseMock
+  }
 }
 
 describe('Login router', () => {
   test('Should return 400 if no email is provided  ', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         password: 'any_password'
@@ -19,7 +30,7 @@ describe('Login router', () => {
   })
 
   test('Should return 400 if no password is provided  ', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = {
       body: {
         email: 'any_email@email.com'
@@ -31,15 +42,28 @@ describe('Login router', () => {
   })
 
   test('Should return 500 if no httpRequest is provided  ', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpResponse = sut.route(undefined)
     expect(httpResponse.statusCode).toBe(500)
   })
 
   test('Should return 500 if httpRequest has no body', () => {
-    const sut = makeSut()
+    const { sut } = makeSut()
     const httpRequest = { undefined }
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
+  })
+
+  test('Should call AuthUseCase with correct params', () => {
+    const { sut, authUseCaseMock } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'any_email@email.com',
+        password: 'any_password'
+      }
+    }
+    sut.route(httpRequest)
+    expect(authUseCaseMock.email).toBe(httpRequest.body.email)
+    expect(authUseCaseMock.password).toBe(httpRequest.body.password)
   })
 })
