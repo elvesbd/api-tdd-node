a223c57d5ffc79f38eeee38b0ba173492766f840
elvesbd <elvesbd@yahoo.com>
2021-11-22 21:43:57 -0300

    feat: ensure LoginRouter returns 200 when valid credentials a provided

--- src/presentation/routers/login-routes.spec.js
+++ src/presentation/routers/login-routes.spec.js
@@ -7,9 +7,11 @@ const makeSut = () => {
     auth (email, password) {
       this.email = email
       this.password = password
+      return this.accessToken
     }
   }
   const authUseCaseMock = new AuthUseCaseMock()
+  authUseCaseMock.accessToken = 'valid-token'
   const sut = new LoginRouter(authUseCaseMock)
   return {
     sut,
@@ -69,7 +71,8 @@ describe('Login router', () => {
   })
 
   test('Should return 401 when invalid credentials are provided', () => {
-    const { sut } = makeSut()
+    const { sut, authUseCaseMock } = makeSut()
+    authUseCaseMock.accessToken = null
     const httpRequest = {
       body: {
         email: 'invalid_email@email.com',
@@ -79,6 +82,18 @@ describe('Login router', () => {
     const httpResponse = sut.route(httpRequest)
     expect(httpResponse.statusCode).toBe(401)
     expect(httpResponse.body).toEqual(new UnauthorizedError())
+  })
+
+  test('Should return 200 when valid credentials are provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@email.com',
+        password: 'valid_password'
+      }
+    }
+    const httpResponse = sut.route(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
   })
 
   test('Should return 500 if no AuthUseCase is provided', () => {
