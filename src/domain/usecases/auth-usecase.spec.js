7c7013b036b422f5e4ddd3dd2c243359bbdc8f5d
elvesbd <elvesbd@yahoo.com>
2021-12-01 18:15:30 -0300

    move sut creation to a factory helper method

--- src/domain/usecases/auth-usecase.spec.js
+++ src/domain/usecases/auth-usecase.spec.js
@@ -16,27 +16,35 @@ class AuthUseCase {
   }
 }
 
+const makeSut = () => {
+  class LoadUserByEmailRepositoryMock {
+    async load (email) {
+      this.email = email
+    }
+  }
+  const loadUserByEmailRepository = new LoadUserByEmailRepositoryMock()
+  const sut = new AuthUseCase(loadUserByEmailRepository)
+  return {
+    sut,
+    loadUserByEmailRepository
+  }
+}
+
 describe('Auth UseCase', () => {
   test('should throw if no email is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth()
     expect(promise).rejects.toThrow(new MissingParamError('email'))
   })
 
   test('should throw if no password is provided', async () => {
-    const sut = new AuthUseCase()
+    const { sut } = makeSut()
     const promise = sut.auth('any_email@gmail.com')
     expect(promise).rejects.toThrow(new MissingParamError('password'))
   })
 
   test('should call LoadUserByEmailRepository with correct email', async () => {
-    class LoadUserByEmailRepositoryMock {
-      async load (email) {
-        this.email = email
-      }
-    }
-    const loadUserByEmailRepository = new LoadUserByEmailRepositoryMock()
-    const sut = new AuthUseCase(loadUserByEmailRepository)
+    const { sut, loadUserByEmailRepository } = makeSut()
     await sut.auth('any_email@gmail.com', 'any_password')
     expect(loadUserByEmailRepository.email).toBe('any_email@gmail.com')
   })
