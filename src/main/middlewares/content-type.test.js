b9549bc177f4251b8acfc6774dfe702074f7c69b
elvesbd <elvesbd@yahoo.com>
2021-12-21 19:41:24 -0300

    reset jest modules in content-type middleware

--- src/main/middlewares/content-type.test.js
+++ src/main/middlewares/content-type.test.js
@@ -1,7 +1,13 @@
 const request = require('supertest')
-const app = require('../config/app')
 
 describe('Content-Type Middlewares', () => {
+  let app
+
+  beforeEach(() => {
+    jest.resetModules()
+    app = require('../config/app')
+  })
+
   test('should return json content-type as default', async () => {
     app.get('/test_content_type', (req, res) => {
       res.send('')
@@ -13,13 +19,13 @@ describe('Content-Type Middlewares', () => {
   })
 
   test('should return xml content-type is forced', async () => {
-    app.get('/test_content_type_xml', (req, res) => {
+    app.get('/test_content_type', (req, res) => {
       res.type('xml')
       res.send('')
     })
 
     await request(app)
-      .get('/test_content_type_xml')
+      .get('/test_content_type')
       .expect('content-type', /xml/)
   })
 })
